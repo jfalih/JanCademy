355682593934ff72b90bd0b870a677749b87cabf
janfalih17 <bosspulsa57@gmail.com>
2021-10-21 12:17:34 +0700

    Add: New Course Detail Page

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,17 +8,98 @@ $(document).ready(function(){
             header.removeClass('scrollActive');
         }
     });
+    // Category Slider Animate
     $('#right-button-category').click(function(e) {
         e.preventDefault();
-        $('#listcategory').animate({
+        $('#list-category').animate({
           scrollLeft: "+=200px"
         });
       });
-      
-       $('#left-button-category').click(function(e) {
+    $('#left-button-category').click(function(e) {
+      e.preventDefault();
+      $('#list-category').animate({
+        scrollLeft: "-=200px"
+      });
+    });
+
+    // Course Slider Animate
+    $('#right-button-course').click(function(e) {
         e.preventDefault();
-        $('#listcategory').animate({
-          scrollLeft: "-=200px"
+        $('#list-course').animate({
+          scrollLeft: "+=200px"
         });
       });
+    $('#left-button-course').click(function(e) {
+      e.preventDefault();
+      $('#list-course').animate({
+        scrollLeft: "-=200px"
+      });
+    });
+    // Question Slider Animate
+    $('#right-button-question').click(function(e) {
+      e.preventDefault();
+      $('#list-question').animate({
+        scrollLeft: "+=200px"
+      });
+    });
+    $('#left-button-question').click(function(e) {
+      e.preventDefault();
+      $('#list-question').animate({
+        scrollLeft: "-=200px"
+      });
+    });
+    
+    // Question Slider Animate
+    $('#right-button-video').click(function(e) {
+      e.preventDefault();
+      $('#list-video').animate({
+        scrollLeft: "+=200px"
+      });
+    });
+    $('#left-button-video').click(function(e) {
+      e.preventDefault();
+      $('#list-video').animate({
+        scrollLeft: "-=200px"
+      });
+    });
+
+    $('#recomendation-course-detail-right').click(function(e) {
+      e.preventDefault();
+      $('#recomendation-course-detail').animate({
+        scrollLeft: "+=200px"
+      });
+    });
+    $('#recomendation-course-detail-left').click(function(e) {
+      e.preventDefault();
+      $('#recomendation-course-detail').animate({
+        scrollLeft: "-=200px"
+      });
+    });
+
+    
+    $('#recomendation-course-1-right').click(function(e) {
+      e.preventDefault();
+      $('#recomendation-course-1').animate({
+        scrollLeft: "+=200px"
+      });
+    });
+    $('#recomendation-course-1-left').click(function(e) {
+      e.preventDefault();
+      $('#recomendation-course-1').animate({
+        scrollLeft: "-=200px"
+      });
+    });
+    
+    $('#recomendation-course-2-right').click(function(e) {
+      e.preventDefault();
+      $('#recomendation-course-2').animate({
+        scrollLeft: "+=200px"
+      });
+    });
+    $('#recomendation-course-2-left').click(function(e) {
+      e.preventDefault();
+      $('#recomendation-course-2').animate({
+        scrollLeft: "-=200px"
+      });
+    });
 })
